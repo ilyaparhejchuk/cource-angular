b2e0a7be3cb9d633c44c409927a962ddfb02f8be
unknown <ilyaparheychuk@gmail.com>
2023-07-26 23:47:37 +0300

    lesson 12

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,17 +1,28 @@
-import { CommonModule } from '@angular/common'
-import { NgModule } from '@angular/core'
-import { RouterModule, Routes } from '@angular/router'
-import { ReactiveFormsModule } from '@angular/forms'
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { ReactiveFormsModule } from '@angular/forms';
 
-import { RegisterComponent } from './components/register/register.component'
+import { RegisterComponent } from './components/register/register.component';
+import { StoreModule } from '@ngrx/store';
+import { reducers } from './store/reducers';
+import { AuthService } from './services/auth.service';
 
-const routes: Routes = [{
+const routes: Routes = [
+  {
     path: 'register',
-    component: RegisterComponent
-}]
+    component: RegisterComponent,
+  },
+];
 
 @NgModule({
-  imports: [CommonModule, RouterModule.forChild(routes), ReactiveFormsModule],
+  imports: [
+    CommonModule,
+    RouterModule.forChild(routes),
+    ReactiveFormsModule,
+    StoreModule.forFeature('auth', reducers),
+  ],
   declarations: [RegisterComponent],
+  providers: [AuthService],
 })
 export class AuthModule {}
--- src/app/auth/components/register/register.component.ts
+++ src/app/auth/components/register/register.component.ts
@@ -1,20 +1,34 @@
-import { Component, OnInit } from '@angular/core'
-import { FormGroup, FormBuilder, Validators } from '@angular/forms'
-import { Store } from '@ngrx/store'
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { Store, select } from '@ngrx/store';
 import { registerAction } from '../../store/actions/register.actions';
+import { Observable, of } from 'rxjs';
+import { isSubmittingSelector } from '../../store/selectors';
+import { AuthService } from '../../services/auth.service';
+import { CurrentUserInterface } from 'src/app/shared/types/currentUser.interface';
 
 @Component({
   selector: 'mc-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.scss']
+  styleUrls: ['./register.component.scss'],
 })
 export class RegisterComponent implements OnInit {
   public form: FormGroup = new FormGroup({});
+  public isSubmitting$!: Observable<boolean>;
 
-  constructor (private fb: FormBuilder, private store: Store) {}
+  constructor(
+    private fb: FormBuilder,
+    private store: Store,
+    private authService: AuthService,
+  ) {}
 
   ngOnInit(): void {
     this.initializeForm();
+    this.initializeValues();
+  }
+
+  public initializeValues(): void {
+    this.isSubmitting$ = this.store.pipe(select(isSubmittingSelector));
   }
 
   public initializeForm(): void {
@@ -22,12 +36,12 @@ export class RegisterComponent implements OnInit {
       username: ['', Validators.required],
       email: ['', Validators.required],
       password: ['', Validators.required],
-    })
+    });
   }
 
   public onSubmit(): void {
-    console.log(this.form.value)
-    this.store.dispatch(registerAction(this.form.value))
+    console.log(this.form.value);
+    this.store.dispatch(registerAction(this.form.value));
+    this.authService.register(this.form.value).subscribe((currentUser: CurrentUserInterface) => console.log(currentUser))
   }
-
 }
